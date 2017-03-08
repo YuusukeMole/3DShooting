27eb01d0e77fc43b73df27dca03d472088f24e99
LAPTOP-TAFL9MMM\syuu1 <s.yuu12.nawate@gmail.com>
2017-03-08 21:40:21 +0900

    3Dのシューティング

---
+++ 3Dshooting/Assets/C#/Barret.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barret : MonoBehaviour 
+{
+	private float lifetime = 1.0f;
+
+	void Update()
+	{
+		lifetime -= Time.deltaTime;
+		if (lifetime < 0) {
+			Destroy (gameObject);
+		}
+	}
+}
